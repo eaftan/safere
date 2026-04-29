f20e879be2d2649087232d52ed7b7d764c7980f9
Eddie Aftandilian <eaftan@users.noreply.github.com>
2026-04-28 23:39:55 -0700

    Re-enable BitState crosscheck regression (#230)
    Fixes #227

--- safere/src/test/java/org/safere/MatcherTest.java
+++ safere/src/test/java/org/safere/MatcherTest.java
@@ -2318,7 +2318,6 @@ class MatcherTest {
   }
 
   @Test
-  @DisabledForCrosscheck("#227 BitState crash for this generated regression case")
   @DisplayName("find() with empty branch before complex char class")
   void findEmptyBranchBeforeComplexCharClass() {
     Matcher m =
