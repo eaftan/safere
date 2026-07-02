e8c1fc5a3c3bf63a8980a165a0952de8e4512d4a
Liam Miller-Cushon <cushon@google.com>
2026-07-02 16:02:23 +0200

    Implement Unicode First-Class Acceleration (DFA Scan Skipping)

--- safere-benchmarks/src/main/java/org/safere/benchmark/RealWorldRegexBenchmark.java
+++ safere-benchmarks/src/main/java/org/safere/benchmark/RealWorldRegexBenchmark.java
@@ -123,7 +123,8 @@ public class RealWorldRegexBenchmark {
     "fruitSearchQuery",
     "fruitMarkupTag",
     "charReplace",
-    "layoutBlock"
+    "layoutBlock",
+    "unicodeClassPrefix"
   })
   public String patternName;
 
